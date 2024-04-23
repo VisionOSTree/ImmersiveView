28e45c5be6fcb6b06248f5f3dadfdf2cdab12366
Bocchi <oce.bocchi@gmail.com>
2024-04-23 12:53:25 +1000

    Initial Commit

---
+++ immersiveTree/immersiveTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  immersiveTreeApp.swift
+//  immersiveTree
+//
+//  Created by Daniel Braithwaite on 23/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct immersiveTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
